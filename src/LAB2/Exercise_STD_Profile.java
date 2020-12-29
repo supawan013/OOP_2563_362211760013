21db813ec5b8c33fa46bfea5373b3b85ffbe6a0c
supawan <supawann1999@gmail.com>
2020-12-29 11:36:21 +0700

    Leb 2 : types and import data

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,27 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new  Scanner(System.in);
+
+    System.out.print("Enter an name: ");
+    String name = scanner.nextLine();
+
+    System.out.print("Enter an std_id: ");
+    String std_id = scanner.next();
+
+    System.out.print("Enter an Age: ");
+    int Age = scanner.nextInt();
+
+    System.out.print("Enter an Tel: ");
+    String Tel = scanner.next();
+
+    System.out.print("Enter an email: ");
+    String email = scanner.next();
+
+    }
+}
